047f87fef95220b2e49b3f0b75625b37dc75c596
Christian Meißner <cme+github@meissner.sh>
2020-10-06 17:39:55 +0200

    Add basic functionality
    * add base query method
    * finalize Api class
    * add login method

--- pyhpipam/__init__.py
+++ pyhpipam/__init__.py
@@ -1,7 +1,7 @@
 from pkg_resources import get_distribution, DistributionNotFound
 
-from pyhpipam.core.query import RequestError, AllocationError, ContentError
 from pyhpipam.core.api import Api as api
+
 
 try:
     __version__ = get_distribution(__name__).version
--- pyhpipam/core/api.py
+++ pyhpipam/core/api.py
@@ -1,1 +1,58 @@
-# provides a common api object to work on.
+# -*- coding: utf-8 -*-
+# (c) Christian Meißner 2020
+
+# pylint: disable=raise-missing-from
+# pylint: disable=super-with-arguments
+
+import requests
+
+from requests.auth import HTTPBasicAuth
+from pyhpipam.core.query import query
+
+GET = requests.get
+POST = requests.post
+PATCH = requests.patch
+OPTIONS = requests.options
+
+
+class Api(object):
+
+    def __init__(
+        self,
+        url,
+        app_id,
+        username=None,
+        password=None,
+        token=None,
+        encryption=False,
+        timeout=None,
+        ssl_verify=True,
+        user_agent=None
+    ):
+        self._api_url = url
+        self._api_appid = app_id
+        self._api_username = username
+        self._api_password = password
+        self._api_token = token
+        self._api_encryption = encryption
+        self._api_timeout = timeout
+        self._api_ssl_verify = ssl_verify
+
+        self._api_headers = {
+            'content-type': 'application/json',
+        }
+
+        if user_agent:
+            self._api_headers['user-agent'] = user_agent
+
+        if not self._api_encryption:
+            self._login()
+
+    def _login(self):
+        _auth = HTTPBasicAuth(self._api_username, self._api_password)
+        resp = query(url=self._api_url, app_id=self._api_appid, method=POST, auth=_auth, verify=self._api_ssl_verify)
+
+        self._token = resp['token']
+
+    def get_token(self):
+        return self._token
