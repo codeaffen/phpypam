c59ae6cc9bbe5b1eeba9f9b5a7c1f96aeb7148fd
Christian Meißner <cme+github@meissner.sh>
2020-10-28 08:18:14 +0100

    release v0.1.5 (#24)
    * return controller paths not names

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="phpypam",
-    version="0.1.4",
+    version="0.1.5",
     author="Christian Meißner",
     author_email="Christian Meißner <cme+codeaffen@meissner.sh>",
     description="Python API client library for phpIPAM installation",
