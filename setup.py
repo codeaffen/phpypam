af1cda013482c1169230d38c02e21a4e72c82f3e
Christian Meißner <cme+github@meissner.sh>
2020-10-30 17:03:16 +0100

    prepare release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="phpypam",
-    version="0.1.7",
+    version="1.0.0",
     author="Christian Meißner",
     author_email="Christian Meißner <cme+codeaffen@meissner.sh>",
     description="Python API client library for phpIPAM installation",
